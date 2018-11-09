7051ffec215cfe379e73cdeb86ac67caeb1e72b8
Warren Argus <warren@warbel.net>
2018-11-09 16:25:16 +0800

    created the cli module and tests

---
+++ tests/test_cli.py
@@ -0,0 +1,34 @@
+import pytest
+
+from pgbackup import cli
+
+url = 'postgres://bob@example.com:5432/db_one'
+
+def test_parser_without_driver():
+    '''
+    Without a specified driver the parser will exit
+    '''
+    with pytest.raises(SystemExit):
+        parser = cli.create_parser()
+        parser.parse_args([url])
+    
+def test_parser_with_driver():
+    '''
+    The parser will exit if it receives a driver without a destination
+    '''
+    parser = cli.create_parser()
+    with pytest.raises(SystemExit):
+        parser.parse_args([url,"--driver","local"])
+
+
+def test_parser_with_driver_and_destination():
+    """
+    The parser will not exist if it receives a driver and destination
+    """
+    parser = cli.create_parser()
+
+    args = parser.parse_args([url, '--driver', 'local','/some/path'])    
+
+    assert args.url == url
+    assert args.driver == 'local'
+    assert args.destination == '/some/path'

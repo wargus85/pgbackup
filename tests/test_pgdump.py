c296451050ca1df02b7f8a8f6568fb9d68a1b853
wargus <warren@warbel.net>
2018-11-11 18:01:48 +0800

    added pgdump.py

--- tests/test_pgdump.py
+++ tests/test_pgdump.py
@@ -10,5 +10,13 @@ def test_dump_calls_pg_dump(mocker):
     Utilise pg_dump wih the database URL
     '''
     mocker.patch('subprocess.Popen')
-    assert pgdump(url)
+    assert pgdump.dump(url)
     subprocess.Popen.assert_called_with(['pg_dump',url], stdout=subprocess.PIPE)
+
+def test_dump_handles_oserror(mocker):
+    '''
+    pgdump.dump returns a reasonable error if pg_dump isn't isntalled
+    '''
+    mocker.patch('subprocess.Popen',side_effect=OSError('no such file'))
+    with pytest.raises(SystemExit):
+        pgdump.dump(url)

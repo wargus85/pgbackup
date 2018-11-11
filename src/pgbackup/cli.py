6769d3c8d79f89e15f6469205ad671a0cc64adf3
Warren Argus <warren@warbel.net>
2018-11-11 10:01:22 +0800

    updated files

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,5 +1,22 @@
-from argparse import ArgumentParser
+from argparse import Action, ArgumentParser
+
+known_drivers = ['local','s3']
+
+class DriverAction(Action):
+    def __call__(self,parser,namespace,values,option_string=None):
+        driver, destination = values
+        if driver.lower() not in known_drivers:
+            parser.error("Unknown driver. Available drivers are 'local' and 's3'")
+        namespace.driver = driver.lower()
+        namespace.destination = destination
 
 def create_parser():
     parser = ArgumentParser()
+    parser.add_argument('url',help="URL of the PostgreSQL database to backup")
+    parser.add_argument('--driver',
+        help="how and where to store the backup",
+        nargs=2,
+        action=DriverAction,
+        required=True
+        )
     return parser
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,13 +4,17 @@ from pgbackup import cli
 
 url = 'postgres://bob@example.com:5432/db_one'
 
-def test_parser_without_driver():
+@pytest.fixture
+def parser():
+        return cli.create_parser()
+
+def test_parser_without_driver(parser):
     '''
     Without a specified driver the parser will exit
     '''
     with pytest.raises(SystemExit):
-        parser = cli.create_parser()
-        parser.parse_args([url])
+       # parser = cli.create_parser() # this line can be commented out from all the tests once we add the pytest.fixture in
+        parser.parse_args([url])      # we then need to pass in the pytest fixture to the test definition
     
 def test_parser_with_driver():
     '''
@@ -20,6 +24,24 @@ def test_parser_with_driver():
     with pytest.raises(SystemExit):
         parser.parse_args([url,"--driver","local"])
 
+def test_parser_with_unknown_driver():
+        '''
+        The parser will exit if the driver name is unknown'
+        '''
+        parser = cli.create_parser()
+
+        with pytest.raises(SystemExit):
+                parser.parse_args([url,'--driver','azure','destination'])
+
+def test_parser_with_known_drivers():
+        '''
+        The parser will not exit if the driver name is known
+        '''
+        parser = cli.create_parser()
+
+        for driver in ['local','s3']:
+            assert parser.parse_args([url,'--driver',driver,'destination'])
+   
 
 def test_parser_with_driver_and_destination():
     """
@@ -32,3 +54,4 @@ def test_parser_with_driver_and_destination():
     assert args.url == url
     assert args.driver == 'local'
     assert args.destination == '/some/path'
+
